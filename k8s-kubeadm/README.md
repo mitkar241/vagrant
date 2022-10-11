76b2aaafacaede5c634bd25a3a368272972a99e3
mitkar241 <raktimhalder241@gmail.com>
2022-10-12 01:58:30 +0530

    Uploaded k8s-kubeadm folder

---
+++ k8s-kubeadm/README.md
@@ -0,0 +1,6 @@
+# Kubernetes - Kubeadm
+---
+
+- [ ] CRI-O runtime - test docker as well
+- [ ] separate master - worker processes
+- [ ] integrate with existing repo
---
+++ k8s-kubeadm/configs/join.sh
@@ -0,0 +1,1 @@
+kubeadm join 192.168.0.10:6443 --token k5n2hr.zrwcmdiy683xfmix --discovery-token-ca-cert-hash sha256:715f05bf0745a26ee802ffbd8ce3c663030841c0456ecbb53056f12f60abbfa1 
